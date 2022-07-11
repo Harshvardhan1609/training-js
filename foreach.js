cabc790e20cde57376ac80a3b3a9f57919cf59c4
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2022-07-11 14:07:40 +0530

    v2

--- foreach.js
+++ foreach.js
@@ -6,5 +6,11 @@ a.push(10)
 console.log(a); 
 a.pop()
 console.log(a);
+a.shift()
+console.log(a)
+//shift will remove th element from the beginning of the array
+a.unshift(40)
+console.log(a);
+//It will add the  element in the first place of the array
 
 //for each will use it to read the same number one only
