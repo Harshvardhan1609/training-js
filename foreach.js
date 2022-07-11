88b7d03f9f9939c328d3ef6708cc18ffd933572f
HARSHVARDHAN PUROHIT <harshvardhanpurohit2020@gmail.com>
2022-07-11 14:04:01 +0530

    for each -2

--- foreach.js
+++ foreach.js
@@ -2,3 +2,9 @@ let a=[12,3,4]
 a.forEach(function(harsh){
     console.log(harsh)
 })
+a.push(10)
+console.log(a); 
+a.pop()
+console.log(a);
+
+//for each will use it to read the same number one only
